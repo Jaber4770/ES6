a3c7223a616c4831c0ed0b64c7a6d84d1ac1a5e0
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-05 15:52:31 +0200

    object

--- day-1/main.js
+++ day-1/main.js
@@ -11,10 +11,49 @@ console.log(findMax(numbers));
 const heros = ["hero Alam", "bapparaj", "joshim", "maruf", "salman khan"];
 const hero = (heros) => {
     let star = heros[0];
-    for(let hero of heros){
+    for (let hero of heros) {
         if (star.length < hero.length)
             star = hero;
     }
     return star;
 }
 console.log(hero(heros));
+
+// object destructuring:
+
+
+const person = {
+    name: "John Doe",
+    age: 30,
+    isStudent: false,
+    hobbies: ["reading", "traveling", "coding"],
+    address: {
+        street: "123 Main St",
+        city: "New York",
+        country: "USA"
+    }
+};
+
+console.log(person.name);
+console.log(Object.keys(person));
+console.log(Object.values(person));
+console.log(person)
+delete person.name;
+console.log(person)
+person.name = "Jack Sargey";
+console.log(person)
+Object.seal(person);
+person.status = "not availalbe"
+console.log(person)
+person.age = 50;
+console.log(person)
+Object.freeze(person);
+person.age = 500;
+console.log(person)
+
+
+
+
+
+
+
