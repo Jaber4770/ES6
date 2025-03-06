7a21c50e433fcec34b7fb2df7e6741eea22ad41d
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-06 02:22:29 +0200

    map, forEach, filter, find,

--- day-2/main.js
+++ day-2/main.js
@@ -52,4 +52,36 @@ const newFriends = friends.map((friend, index) => {
 
 
 
+// ----31-4 foreach, filter, find, and differences between them
+
+const phones = [
+    { id: 1, name: "iPhone 15 Pro", color: "Black Titanium", price: 999, brand: "Apple" },
+    { id: 2, name: "Samsung Galaxy S24 Ultra", color: "Titanium Gray", price: 1199, brand: "Samsung" },
+    { id: 3, name: "Google Pixel 8 Pro", color: "Obsidian", price: 899, brand: "Google" },
+    { id: 4, name: "OnePlus 12", color: "Eternal Green", price: 799, brand: "OnePlus" },
+    { id: 5, name: "Xiaomi 14 Pro", color: "Black", price: 699, brand: "Xiaomi" },
+    { id: 6, name: "Sony Xperia 1 V", color: "Frosted Black", price: 1199, brand: "Sony" },
+    { id: 7, name: "Asus ROG Phone 8", color: "Phantom Black", price: 1099, brand: "Asus" },
+    { id: 8, name: "Nothing Phone 2", color: "Transparent", price: 599, brand: "Nothing" },
+    { id: 9, name: "Realme GT 5", color: "Pulse White", price: 549, brand: "Realme" },
+    { id: 10, name: "Motorola Edge 40 Pro", color: "Nebula Blue", price: 799, brand: "Motorola" }
+];
+
+phones.forEach(phone => {
+    if (phone.price >= 800) {
+        console.log(phone);
+    }
+});
+
+const filter = phones.filter( phone => phone.price > 1000);
+console.log(filter);
+
+phones.forEach(phone => {
+    console.log(phone.name, phone.price);
+});
+
+const sony = phones.find(phone => phone.name.includes("Sony"));
+console.log(sony);
+
+console.log(phones.find(p=> p.id===4))
 
