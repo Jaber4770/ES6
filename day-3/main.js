31d63e888c3b6e6f62bfabf2d4b31be8e649b555
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-10 01:34:01 +0200

    fetch done

--- day-3/main.js
+++ day-3/main.js
@@ -1,2 +1,6 @@
-console.log(5*"2");
-console.log(5-"2");
+console.log(5 * "2");
+console.log(5 - "2");
+
+fetch('https://jsonplaceholder.typicode.com/todos/1')
+    .then(response => response.json())
+    .then(json => console.log(json))
