ead2c4790c37861bd2c440f6b1966709e9cd8ddd
Jaber Ahmed <jaber.ahmed4770@gmail.com>
2025-03-10 03:21:44 +0200

    show user name done

--- day-3/main.js
+++ day-3/main.js
@@ -1,6 +1,18 @@
 console.log(5 * "2");
 console.log(5 - "2");
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json())
-    .then(json => console.log(json))
+
+const container = document.getElementById('container');
+
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => showUser(data));
+
+
+const showUser = (users) => {
+    for (let user of users) {
+        const li = document.createElement('li');
+        li.innerText = user.name;
+        container.appendChild(li);
+    }
+}
